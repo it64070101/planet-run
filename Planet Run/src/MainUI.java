c625761ac2a2d3e89776b572c2606be0d99d1d99
Jeerawat Maiwai <102474034+JeerawatM@users.noreply.github.com>
2022-12-14 22:47:06 +0700

    add

--- Planet Run/src/MainUI.java
+++ Planet Run/src/MainUI.java
@@ -16,7 +16,8 @@ import javax.swing.JInternalFrame;
 
 public class MainUI extends JPanel {
     private JFrame fr;
-    private JPanel pAll, pShip, pHuntingGround, pSectors, pGadget, pRest, pLabel, pButton, pMaketool, pComponent;
+    private JPanel pAll, pShip, pHuntingGround, pSectors, pGadget, pRest, pLabel, pButton, pMaketool, pComponent,
+            pRocket;
     private JButton noseCone, shockcord, recoveryWadding, leftFin, rightFin;
     private JButton sectorA, sectorB, sectorC, sectorD, rest, maketool;
     private JButton huntingGround0, huntingGround1;
@@ -26,10 +27,9 @@ public class MainUI extends JPanel {
     private JDesktopPane dp1;
     private JInternalFrame inf1, inf2, inf3, inf4, inf5, inf6, inf7;
 
-    
-    public MainUI(){
+    public MainUI() {
         fr = new JFrame("Planet Run");
-        
+
         pAll = new JPanel();
         pShip = new JPanel();
         pHuntingGround = new JPanel();
@@ -39,12 +39,13 @@ public class MainUI extends JPanel {
         pLabel = new JPanel();
         pButton = new JPanel();
         pComponent = new JPanel();
+        pRocket = new JPanel();
         rockcone = new ImageIcon("rockcone.png");
         rockleft = new ImageIcon("rockleft.png");
         rockright = new ImageIcon("rockright.png");
         rockbase = new ImageIcon("rockbase.png");
         rockbody = new ImageIcon("rockbody.png");
-        
+
         BorderLayout layout01 = new BorderLayout();
         // layout01.setBackground(Color.BLACK);
 
@@ -53,14 +54,14 @@ public class MainUI extends JPanel {
         recoveryWadding = new JButton(rockbase);
         leftFin = new JButton(rockleft);
         rightFin = new JButton(rockright);
-        
+
         noseCone.setPreferredSize(new Dimension(100, 100));
         shockcord.setPreferredSize(new Dimension(150, 200));
         recoveryWadding.setPreferredSize(new Dimension(100, 100));
         leftFin.setPreferredSize(new Dimension(100, 100));
         rightFin.setPreferredSize(new Dimension(100, 100));
-        
-        pShip.setLayout(new BorderLayout(5,5));
+
+        pShip.setLayout(new BorderLayout(5, 5));
         pShip.setBackground(Color.BLACK);
         pShip.setSize(128, 128);
         pShip.add(noseCone, BorderLayout.NORTH);
@@ -69,26 +70,26 @@ public class MainUI extends JPanel {
         pShip.add(leftFin, BorderLayout.WEST);
         pShip.add(rightFin, BorderLayout.EAST);
         // noseCone.setBackground(Color.BLACK);
-        
+
         sectorA = new JButton("Explore this Sector");
         sectorB = new JButton("Explore this Sector");
         sectorC = new JButton("Explore this Sector");
         sectorD = new JButton("Explore this Sector");
         rest = new JButton("Rest");
         maketool = new JButton("Make Tool");
-        
+
         rest.setPreferredSize(new Dimension(100, 50));
         maketool.setPreferredSize(new Dimension(100, 50));
 
-        pSectors.setLayout(new GridLayout(2,2));
+        pSectors.setLayout(new GridLayout(2, 2));
         pSectors.add(sectorA);
         pSectors.add(sectorB);
         pSectors.add(sectorC);
         pSectors.add(sectorD);
-        
+
         huntingGround0 = new JButton("Fight this Animal");
         huntingGround1 = new JButton("Fight this Animal");
-        
+
         pHuntingGround.setLayout(new GridLayout(2, 1));
         pHuntingGround.add(huntingGround0);
         pHuntingGround.add(huntingGround1);
@@ -100,14 +101,14 @@ public class MainUI extends JPanel {
         squareTxt = new JTextField("Square: 0");
         circleTxt = new JTextField("Circle: 0");
         title1 = new JTextField("Planet Run!");
-        
+
         pLabel.add(dayTxt);
         pLabel.add(foodTxt);
         pLabel.add(starTxt);
         pLabel.add(triangleTxt);
         pLabel.add(squareTxt);
         pLabel.add(circleTxt);
-        
+
         dayTxt.setEditable(false);
         foodTxt.setEditable(false);
         starTxt.setEditable(false);
@@ -116,20 +117,21 @@ public class MainUI extends JPanel {
         circleTxt.setEditable(false);
 
         dp1 = new JDesktopPane();
-        dp1.setSize(200,200);
+        dp1.setSize(200, 200);
         inf1 = new JInternalFrame("Rocket", false, true, true, true);
         // inf2 = new JInternalFrame();
         // inf3 = new JInternalFrame();
         // inf4 = new JInternalFrame();
         // inf5 = new JInternalFrame();
-        
+
         // inf1.setLayout(new FLowLayout());
-        // inf1.add(pComponent);
-        // inf1.add(maketool, BorderLayout.NORTH);
-        // inf1.add(pShip, BorderLayout.CENTER);
-        // inf1.add(rest, BorderLayout.SOUTH);
+        inf1.add(pComponent);
+        inf1.add(maketool, BorderLayout.NORTH);
+        inf1.add(pShip, BorderLayout.CENTER);
+        inf1.add(rest, BorderLayout.SOUTH);
         ((javax.swing.plaf.basic.BasicInternalFrameUI) inf1.getUI()).setNorthPane(null);
-        inf1.setSize(500, 500);
+        // inf1.setSize(500, 500);
+        inf1.pack();
         inf1.setMaximumSize(new Dimension(500, 500));
         inf1.setLocation(200, 280);
         // inf1.getContentPane().add(new JLabel("rocket"));
@@ -137,14 +139,15 @@ public class MainUI extends JPanel {
         // inf1.pack();
         inf1.setVisible(true);
 
-        pButton.setLayout(new BorderLayout(5,5));
+        pButton.setLayout(new BorderLayout(5, 5));
         pButton.setBackground(Color.BLACK);
         pButton.add(maketool, BorderLayout.NORTH);
         pButton.add(pShip, BorderLayout.CENTER);
         pButton.add(rest, BorderLayout.SOUTH);
-        
+
         pAll.setLayout(new FlowLayout(FlowLayout.CENTER));
         pAll.add(pLabel);
+        pAll.setSize(100, 100);
 
         pComponent.setLayout(new BorderLayout(100, 100));
         pComponent.setBackground(Color.BLACK);
@@ -154,42 +157,45 @@ public class MainUI extends JPanel {
         // pComponent.add(pComponent);
         inf1.add(pComponent);
         dp1.add(inf1);
-        
 
+        // rocket panel
+        pRocket.setLayout(new BorderLayout(5,40));
+        pRocket.add(pAll, BorderLayout.NORTH);
+        pRocket.add(inf1, BorderLayout.SOUTH);
+        pRocket.setBackground(new Color(0,0,0,0));
 
         pAll.setLocation(30, 30);
-        fr.setLayout(layout01);
-        fr.add(pLabel, BorderLayout.NORTH);
-        fr.add(dp1, BorderLayout.CENTER);
+        fr.setLayout(new FlowLayout());
+        // fr.add(pAll);
+        fr.add(pRocket);
+        fr.add(pRocket);
         // fr.add(pComponent);
-        fr.getContentPane().setBackground(Color.BLACK);
+        fr.getContentPane().setBackground(Color.darkGray);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fr.setSize(1080, 960);
         // fr.pack();
         fr.setLocationRelativeTo(null);
         fr.setVisible(true);
         // fr.setResizable(false);
-        
-    }
 
-        
+    }
 
     public static void main(String[] args) {
         new MainUI();
     }
 
-    public void paintComponent(Graphics g){
-        Graphics2D g2 = (Graphics2D) g; //cast for java2
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g; // cast for java2
         // my graphics:
         g2.setColor(Color.GREEN);
         // g2.drawCircle(g, 30, 30, 20);
-        g2.fillRect(10,10,200,50);
+        g2.fillRect(10, 10, 200, 50);
         // g2.setColor(new Color(0,0,255));
         g2.drawString("Hello World", 10, 10);
 
         super.paintComponent(g);
-        }
+    }
     // public void drawCircle(Graphics cg, int xCenter, int yCenter, int r) {
-    //     cg.drawOval(xCenter-r, yCenter-r, 2*r, 2*r);
+    // cg.drawOval(xCenter-r, yCenter-r, 2*r, 2*r);
     // }
 }
