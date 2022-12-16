dd61078494f4bdc7a489574e3750c24b756a8ff0
64070167 <88415664+Squid3rd@users.noreply.github.com>
2022-12-16 23:22:51 +0700

    55

--- Planet Run/src/MainHome.java
+++ Planet Run/src/MainHome.java
@@ -38,14 +38,13 @@ public class MainHome extends JPanel implements ActionListener {
         p1.add(bStart); p1.add(bConti); 
         p1.add(bHow); p1.add(bExit); 
 
-        // p2.setSize(200,200);;
+        // p2.setSize(200,200);
         p2.setBorder(new EmptyBorder(100, 10, 10, 10));
         title1.setFont(new Font("Ink Free", Font.BOLD, 50));
         title1.setForeground(new Color(0,255,255));
         p2.add(title1);
 
         fr.setLayout(new FlowLayout());
-        // fr.add(p1, BorderLayout.LINE_END);
         fr.add(p2, BorderLayout.NORTH);       
         fr.add(p1, BorderLayout.PAGE_END);
         // fr.add(p);
@@ -72,14 +71,8 @@ public class MainHome extends JPanel implements ActionListener {
         fr.setLocationRelativeTo(null);
         fr.setVisible(true);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // fr.add(test);
         fr.add(test);
-        // fr.add(test);
-        // fr.add(test);
         fr.add(p);
-        // fr.pack();
-        // fr.addKeyListener(p);
-
         fr.setVisible(true);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -102,11 +95,8 @@ public class MainHome extends JPanel implements ActionListener {
 
     public void paintComponent(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
-        ani = new ImageIcon("images/Ani1.png").getImage();
+        ani = new ImageIcon("src/images/Ani1.png").getImage();
         backgroundimg = new ImageIcon("images/spacetest.jpg").getImage();
-        // Image n1 = backgroundimg.getScaledInstance(1600,900, Image.SCALE_SMOOTH);
-        // Image n2 = new ImageIcon(n1);
-        // g2D.drawString("MEEEEEEEEEEEEE", 100, 100);
         g2D.drawImage(backgroundimg, 0, 0, null);
         g2D.drawImage(ani, 1,1, null);
         }
